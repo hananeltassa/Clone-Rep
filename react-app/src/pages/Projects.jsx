b9a58373b51c3be09c387bc72dbf84a90d8c568f
hananeltassa <hananeltassa15@gmail.com>
2024-12-02 11:26:51 +0200

    Fixed bugs in react

--- react-app/src/pages/Projects.jsx
+++ react-app/src/pages/Projects.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import Project from "../components/Project";
 
 const Projects = () => {
+  const [projects] = useState([]); 
+
   return (
     <div className="projects-container">
+      <h1>hi</h1>
       {projects.map((p) => (
         <Project project={p} key={p.id} />
       ))}
